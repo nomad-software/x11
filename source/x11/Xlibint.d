82fa83a7b9e5f883aa1c32ea2542a1deaabc5f9d
Geod24 <mathias.lang@epitech.eu>
2014-04-14 19:53:14 -0700

    Fixing the Atom bug

--- source/x11/Xlibint.d
+++ source/x11/Xlibint.d
@@ -11,9 +11,9 @@ import std.c.string : memcpy;
 import core.stdc.config;
 import core.stdc.stdlib : free, malloc, calloc, realloc;
 
-import x11.X    :   XID;
+import x11.X    : XID, GContext, KeySym, Font, VisualID, Window;
 import x11.Xmd  : CARD32;
-import x11.Xlib :   _XrmHashBucketRec, Bool,Screen, ScreenFormat, Status, Visual, XChar2b, XCharStruct,
+import x11.Xlib : _XrmHashBucketRec, Bool,Screen, ScreenFormat, Status, Visual, XChar2b, XCharStruct,
                     XConnectionWatchProc, XEvent, XErrorEvent, XExtCodes, XExtData, XFontStruct, XGCValues,
                     XGenericEventCookie, XModifierKeymap, XPointer, XRectangle, XSetWindowAttributes, XWindowAttributes;
 import x11.Xtos;
